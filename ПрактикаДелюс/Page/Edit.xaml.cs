e0f052cba007fc4fcb6d1a9a4c23211d643eec69
232007 <232007@K1210-13>
2023-03-10 14:36:35 +0300

    Добавьте файлы проекта.

---
+++ ПрактикаДелюс/Page/Edit.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПрактикаДелюс.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для Edit.xaml
+    /// </summary>
+    public partial class Edit : Page
+    {
+        public Edit()
+        {
+            InitializeComponent();
+        }
+
+        private void NextAddProd_click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void ClearAddProd_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddProd_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ПрактикаДелюс/Page/List.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПрактикаДелюс.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для List.xaml
+    /// </summary>
+    public partial class List
+    {
+        public List()
+        {
+            InitializeComponent();
+        }
+
+        private void podrobnee_Click(object sender, RoutedEventArgs e)
+        {
+            //var BtnProd = (sender as Button).DataContext as Product;
+            //UserAuth.UserAuth.tovar = Dbconnect.db.Product.Where(x => x.ID == BtnProd.ID).FirstOrDefault();
+
+            //NavigationService.Navigate(new RedAdminList(UserAuth.UserAuth.tovar));
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {   
+            NavigationService.Navigate(new Add());
+        }
+    }
+}
